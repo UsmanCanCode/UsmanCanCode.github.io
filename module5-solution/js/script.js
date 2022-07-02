36bad16347b0d8c9695d83ad02082e419363d7fa
UsmanCanCode <86849038+UsmanCanCode@users.noreply.github.com>
2022-07-02 17:57:20 -0500

    updates

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -103,7 +103,6 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
-
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -115,15 +114,15 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-        var propValue = "{{" + chosenCategoryShortName + "}}";
-       homeHtml = insertProperty(homeHtml, "randomCategoryShortName", propValue );
+        var propValue = "\" + chosenCategoryShortName + \"";
+       var homeHtmlInsert = insertProperty(homeHtml, "randomCategoryShortName", propValue );
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("main-content", homeHtml);
+      insertHtml("main-content", homeHtmlInsert);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
